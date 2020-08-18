c0975b2c76bd1c32a89747be4b1c235aef223503
unknown <michaelangelomotta@gmail.com>
2020-08-18 12:55:30 -0400

    Card optimization

--- src/components/AnimeItem.js
+++ src/components/AnimeItem.js
@@ -3,25 +3,42 @@ import React from 'react';
 const AnimeItem = ({ anime }) => {
   return (
     <div className="card">
-      <h2 className="card-title">{anime.title}</h2>
-      <img src={anime.image_url} alt="" className="card-img" />
+      <a href={anime.url}>
+        <h2 className="card-title">{anime.title}</h2>
+
+        <img
+          src={anime.image_url}
+          alt="Anime poster image"
+          className="card-img"
+        />
+      </a>
       <div className="card-details">
         <p>
           <strong>Score:</strong> {anime.score}
         </p>
         {anime.type === 'TV' && (
           <p>
-            <strong>Episodes:</strong> {anime.episodes}
+            <strong>Episodes: </strong> {anime.episodes}
           </p>
         )}
-        <p>
-          <strong>Start Date:</strong>
-          {anime.start_date}
-        </p>
-        <p>
-          <strong>End Date:</strong>
-          {anime.end_date}
-        </p>
+
+        {!anime.start_date ? (
+          <p></p>
+        ) : (
+          <p>
+            <strong>Start Date: </strong>
+            {anime.start_date}
+          </p>
+        )}
+
+        {!anime.end_date ? (
+          <p></p>
+        ) : (
+          <p>
+            <strong>End Date: </strong>
+            {anime.end_date}
+          </p>
+        )}
         <p>
           {anime.type === 'Movie' ? (
             <strong>Movie</strong>
@@ -29,6 +46,15 @@ const AnimeItem = ({ anime }) => {
             <strong>TV Show</strong>
           )}
         </p>
+
+        {!anime.rated ? (
+          <p></p>
+        ) : (
+          <p>
+            <strong>Rated: </strong>
+            {anime.rated}
+          </p>
+        )}
       </div>
     </div>
   );
